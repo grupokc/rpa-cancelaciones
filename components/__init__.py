736604d658e6e66ee32d9fc96f14b1378c5dc53d
M Casarin <mauricio.casarin@grupokc.com.mx>
2025-08-26 12:44:20 -0600

    First Commit

---
+++ components/__init__.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import (
+    QMainWindow, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem,
+    QPushButton, QLabel, QFileDialog, QMessageBox, QTabWidget
+)
+from PySide6 import Qt 
+from PySide6.QtCore import Qt, QDate
+from PySide6.QtGui import QColor
+
---
+++ components/main_window.py
@@ -0,0 +1,8 @@
+from components import * 
+
+
+
+class MainWindow(QMainWindow): 
+    def __init__(self): 
+        super().__init__()
+        self.setWindowTitle("RPA Grupo Kc")
